2ba849845f25610c2d4ca49a08c9c6c6d7ebffaf
rakshita018 <rakshitha.23cs113@sode-edu.in>
2024-09-26 10:20:38 +0530

    Add files via upload

---
+++ Teststack.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+class Stack {
+    int top;
+    int stk[]=new int[10];
+    Stack(){
+        top=-1;}
+    void push(int item){
+        if(stk.length-1==top){
+            System.out.println("Stack overflow");}
+        else{
+            top=top+1;
+            stk[top]=item;}}
+    void pop(){
+        if(top==-1){
+            System.out.println("Stack underflow");}
+        else{
+            int item=stk[top];
+            System.out.println("Item popped is:" +item);
+            top=top-1;}}
+    void display(){
+        
+        if(top==-1){
+            System.out.println("Stack is empty. No items to display");}
+        else{
+            System.out.println("Items in the stack are");     
+            for(int i=top;i>=0;i--){
+            System.out.println(stk[i]);
+                    }
+                }
+            }
+        }
+ public class Teststack{
+     public static void main(String[] args){
+             int ch;
+             Stack stack1=new Stack();
+             Scanner scl=new Scanner(System.in);
+             while(true){
+                 System.out.println("1: Push 2: Pop 3:Display 4:Exit");
+                 ch=scl.nextInt();
+                 switch(ch){
+                     case 1:
+                         System.out.println("Enter the items to be pushed");
+                         int item=scl.nextInt();
+                         stack1.push(item);
+                        break;
+                        
+                     case 2:
+                         stack1.pop();
+                         break;
+                         
+                     case 3:
+                         stack1.display();
+                         break;
+                         
+                     case 4:
+                         System.exit(0);
+                         
+                     default:
+                         System.out.println("Enter the valid choice");
+                 }
+             }     
+        }
+                 
+    }
